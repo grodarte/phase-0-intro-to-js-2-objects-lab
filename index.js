ec4e77b4c6bdb43f1c3ef29087903dd5d60b2f44
Gabrielle <ms.grodarte@gmail.com>
2024-01-11 12:44:49 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Gabrielle",
+    streetAddress: "tbd"
+}
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {
+        ...object,
+        [key]: value,
+    }
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value
+    return object
+    }
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee = {
+        ...object,
+    }
+    delete newEmployee.name
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete employee.name
+    return employee
+}
